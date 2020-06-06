7917b43db9030aafab2b5530495b1047adf9d71e
Vinicius Dias <vinicius-mdias@hotmail.com>
2020-06-06 15:35:46 -0300

    change redirect url

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ export const config = {
   spotify: {
     authorizationURL: 'https://accounts.spotify.com/authorize',
     clientId: '4ac48558c5d0421da843b642d630f9e1',
-    redirectUrl: `${window.location.origin}/authorize`,
+    redirectUrl: `https://viniciusdias-spotify.netlify.app/callback`,
     webAPI: 'https://api.spotify.com/v1',
     scopes: [
       'user-read-email',
