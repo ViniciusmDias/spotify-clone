c03552f0523a103d9c8a315478c1e9dd6c053d60
Vinicius Dias <vinicius-mdias@hotmail.com>
2020-06-06 15:19:26 -0300

    Add project to Github

---
+++ build/precache-manifest.c26906f061d6890862ca20f88f2c1dfc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9af0615f5e849b14c9bafb13379d40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fdceb9a0f1e0beff82d",
+    "url": "/static/css/main.80f71c81.chunk.css"
+  },
+  {
+    "revision": "731c0fef79303605a296",
+    "url": "/static/js/2.a84fd253.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.a84fd253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fdceb9a0f1e0beff82d",
+    "url": "/static/js/main.ef656e99.chunk.js"
+  },
+  {
+    "revision": "f29cea98986c271ac980",
+    "url": "/static/js/runtime-main.11507720.js"
+  },
+  {
+    "revision": "60df5f786342c4271a34485e660dfa29",
+    "url": "/static/media/app-intro-1.60df5f78.jpg"
+  },
+  {
+    "revision": "ac2b9dd2bc9787d57fa5e5ea5ffc5e80",
+    "url": "/static/media/app-intro-2.ac2b9dd2.jpg"
+  },
+  {
+    "revision": "eae1132707e6db0e137a298ceac30bf7",
+    "url": "/static/media/spotify-logo-green.eae11327.svg"
+  }
+]);
---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+export const config = {
+  spotify: {
+    authorizationURL: 'https://accounts.spotify.com/authorize',
+    clientId: '4ac48558c5d0421da843b642d630f9e1',
+    redirectUrl: `${window.location.origin}/authorize`,
+    webAPI: 'https://api.spotify.com/v1',
+    scopes: [
+      'user-read-email',
+      'user-read-private',
+      'streaming'
+    ]
+  }
+}
