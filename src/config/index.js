6e8d9e78488400a0ee0aa84f241adba61895a051
Vinicius Dias <vinicius-mdias@hotmail.com>
2020-06-06 15:58:15 -0300

    Fix playlist error and change redirectUrl

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ export const config = {
   spotify: {
     authorizationURL: 'https://accounts.spotify.com/authorize',
     clientId: '4ac48558c5d0421da843b642d630f9e1',
-    redirectUrl: `https://viniciusdias-spotify.netlify.app/callback`,
+    redirectUrl: `https://viniciusdias-spotify.netlify.app/authorize`,
     webAPI: 'https://api.spotify.com/v1',
     scopes: [
       'user-read-email',
--- src/containers/Playlists/Playlists.jsx
+++ src/containers/Playlists/Playlists.jsx
@@ -1,21 +1,50 @@
-import React from 'react';
+import React  from 'react';
+import PropTypes from 'prop-types';
 
-import {
-  Route,
-  Redirect,
-} from "react-router-dom";
-import { useSelector } from 'react-redux';
+import { Loading, RouteHeader } from '../../components';
+import PlaylistItem from './PlaylistItem';
 
-const PrivateRoute = ({ comp: Component, ...rest }) => {
-  const isLogged = useSelector(state => state.auth.isLogged);
+import './Playlists.scss';
 
-  return (
-    <Route {...rest} render={props =>
-      !isLogged
-        ? (<Redirect to="/" />)
-        : (<Component {...props} />)
-    } />
-  );
+const Playlists = ({ data, categoryName, categoryId, isLoading, path }) => (
+  <div className="playlists" data-testid="playlists">
+    <div className="container">
+      <RouteHeader
+        categoryName={categoryName}
+        path={path}
+      />
+
+      {isLoading
+        ? (<Loading text="Carregando playlists..."/>)
+        : (
+          <div className="playlists__content">
+            {data.length && data.map(playlist => (
+              <PlaylistItem
+                categoryId={categoryId}
+                description={playlist.description}
+                id={playlist.id}
+                image={playlist.images[0]}
+                key={playlist.id}
+                name={playlist.name}
+                path={path}
+              />
+            ))}
+          </div>
+        )}
+    </div>
+  </div>
+);
+
+Playlists.defaultProps = {
+  isLoading: false,
 }
 
-export default PrivateRoute;
+Playlists.propTypes = {
+  categoryId: PropTypes.string.isRequired,
+  categoryName: PropTypes.string.isRequired,
+  data: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool,
+  path: PropTypes.string.isRequired,
+}
+
+export default Playlists;
